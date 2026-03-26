d784a6b04c8023dc328595b8f4129a051e05d7f4
oguz <oguzhankir17@gmail.com>
2026-03-26 22:52:37 +0300

    chore: update dependencies in Cargo.toml and Cargo.lock to latest versions, including pyo3 and heck; refactor function signatures in lib.rs to use Bound for better memory safety.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,42 +5,42 @@ mod histogram;
 mod utf8;
 
 #[pyfunction]
-fn byte_histogram(data: &PyBytes) -> Vec<u32> {
+fn byte_histogram(data: &Bound<'_, PyBytes>) -> Vec<u32> {
     histogram::byte_histogram(data.as_bytes()).to_vec()
 }
 
 #[pyfunction]
-fn utf8_check(data: &PyBytes) -> (bool, f64) {
+fn utf8_check(data: &Bound<'_, PyBytes>) -> (bool, f64) {
     utf8::utf8_check(data.as_bytes())
 }
 
 #[pyfunction]
-fn utf8_continuation_score(data: &PyBytes) -> f64 {
+fn utf8_continuation_score(data: &Bound<'_, PyBytes>) -> f64 {
     utf8::utf8_continuation_score(data.as_bytes())
 }
 
 #[pyfunction]
-fn high_byte_ratio(data: &PyBytes) -> f64 {
+fn high_byte_ratio(data: &Bound<'_, PyBytes>) -> f64 {
     histogram::high_byte_ratio(data.as_bytes())
 }
 
 #[pyfunction]
-fn null_byte_ratio(data: &PyBytes) -> f64 {
+fn null_byte_ratio(data: &Bound<'_, PyBytes>) -> f64 {
     histogram::null_byte_ratio(data.as_bytes())
 }
 
 #[pyfunction]
-fn cp1252_zone_ratio(data: &PyBytes) -> f64 {
+fn cp1252_zone_ratio(data: &Bound<'_, PyBytes>) -> f64 {
     histogram::cp1252_zone_ratio(data.as_bytes())
 }
 
 #[pyfunction]
-fn detect_null_pattern(data: &PyBytes) -> i32 {
+fn detect_null_pattern(data: &Bound<'_, PyBytes>) -> i32 {
     utf8::detect_null_pattern(data.as_bytes())
 }
 
 #[pymodule]
-fn _rust_core(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+fn _rust_core(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(byte_histogram, m)?)?;
     m.add_function(wrap_pyfunction!(utf8_check, m)?)?;
     m.add_function(wrap_pyfunction!(utf8_continuation_score, m)?)?;
